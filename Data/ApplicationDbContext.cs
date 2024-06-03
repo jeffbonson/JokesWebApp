903a479ef27ce64282a3e384f762a41f9c6d21e2
jeffbonson <jeffbonson@gmail.com>
2024-06-03 11:15:49 +0530

    adding jokes crud

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using JokesWebApp.Models;
 
 namespace JokesWebApp.Data
 {
@@ -9,5 +10,6 @@ namespace JokesWebApp.Data
             : base(options)
         {
         }
+        public DbSet<JokesWebApp.Models.Joke> Joke { get; set; } = default!;
     }
 }
---
+++ Models/Joke.cs
@@ -0,0 +1,14 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int id { get; set; }
+        public string question { get; set; }
+        public string answer { get; set; }
+
+        public Joke()
+        {
+                
+        }
+    }
+}
